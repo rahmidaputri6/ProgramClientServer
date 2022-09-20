a41e79ec230686eeef36eb30af61e58062d36648
Rahmida putri <113493930+rahmidaputri6@users.noreply.github.com>
2022-09-20 13:54:19 +0700

    Add files via upload
    jdk-17.0.4.1
    boot string 2.7.3
    packaging jar

---
+++ latihan2-service/src/test/java/com/rahmidaputri/latihan2/service/Latihan2ServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.rahmidaputri.latihan2.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootTest
+@RestController
+class Latihan2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
